d66302d7a8e411587d8550bc1db8aad3d203b9e6
nijuthomas <niju.thomas.niju@gmail.com>
2025-01-26 14:30:50 +0000

    nextjs Tutorial

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,12 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
+
+export default function Page1() {
+  return (
+    <div className="flex min-h-screen flex-col p-6">
+        Invoices page
+    </div>
+  );
+}
